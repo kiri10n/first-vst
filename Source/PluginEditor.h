d4966a3efd55c04a4ec34ce337ee51bfe4108277
kiritohaze <kiritohaze@gmail.com>
2024-03-07 15:56:20 +0900

    add sliders for UI

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -28,6 +28,17 @@ private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     FirstVSTAudioProcessor& audioProcessor;
+    
+    typedef juce::AudioProcessorValueTreeState::SliderAttachment SliderAttachment;
+    typedef juce::AudioProcessorValueTreeState::ComboBoxAttachment ComboBoxAttachment;
+    
+    juce::AudioProcessorValueTreeState& valueTreeState;
+    juce::Slider gainSlider;
+    std::unique_ptr<SliderAttachment> gainSliderAttachment;
+    juce::Slider panAngleSlider;
+    std::unique_ptr<SliderAttachment> panAngleSliderAttachment;
+    juce::ComboBox panRuleBox;
+    std::unique_ptr<ComboBoxAttachment> panRuleBoxAttachment;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (FirstVSTAudioProcessorEditor)
 };
